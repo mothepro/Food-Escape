edbd2e8c6fee48751fddebf55afe246a9fe49f6a
Maurice Prosper <maurice.prosper@ttu.edu>
2015-02-20 03:21:57 -0600

    Initial
    This is the demo which Cole has worked on so far

---
+++ Assets/Door.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	public static bool key_found = false;
+	public float waitTime = 2f;
+
+	void OnMouseDown() {
+		if (key_found) {
+			print ("Clicked");
+			StartCoroutine (gamePauseTime ());
+			GetComponent<SpriteRenderer> ().color = Color.black;
+		} else
+			print ("Need Key");
+
+	}
+
+	IEnumerator gamePauseTime() {
+		yield return new WaitForSeconds (waitTime);
+		openDoor ();
+	}
+
+	void openDoor() {
+		Application.LoadLevel ("_Scene_1");
+	}
+
+}
---
+++ Assets/Key.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Key : MonoBehaviour {
+
+	void OnMouseDown() {
+		Door.key_found = true;
+		Destroy (gameObject);
+
+	}
+}
